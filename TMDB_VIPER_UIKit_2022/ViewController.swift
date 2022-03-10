6ad7724de4c5d6f27584a0936d95445475662e51
The Handy Owl <thehandyowl.io@gmail.com>
2022-03-10 11:25:50 +0100

    Starting new project

---
+++ TMDB_VIPER_UIKit_2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB_VIPER_UIKit_2022
+//
+//  Created by Carlos on 10/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
